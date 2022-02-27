b307df8f8052ddfe2b63e0122d1421b051b219fb
jjh <joojinhyun00@gmail.com>
2022-02-27 18:33:09 +0900

    추상 클래스

--- src/main/java/com/example/springbootdemo/domain/BaseTimeEntity.java
+++ src/main/java/com/example/springbootdemo/domain/BaseTimeEntity.java
@@ -14,7 +14,7 @@ import lombok.Getter;
 @Getter
 @MappedSuperclass // JPA 엔티티 클래스가 BaseTimeEntity를 상속할 경우 필드(createdTime, modifiedTime)도 칼럼으로 인식하도록 함
 @EntityListeners(AuditingEntityListener.class) // BaseTimeEntity 클래스에 Auditing 기능을 포함
-public class BaseTimeEntity {
+public abstract class BaseTimeEntity {
     
     @CreatedDate // 엔티티가 생성되어 저장될 때 시간이 자동 저장
     private LocalDateTime createdTime;
