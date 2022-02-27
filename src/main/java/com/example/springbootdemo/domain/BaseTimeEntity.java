70209d98db523066faeb879e42f68815a6aa2af9
jjh <joojinhyun00@gmail.com>
2022-02-27 18:21:40 +0900

    엔티티들의 createdTime, modifiedTime 필드를 자동으로 관리하는 역할

---
+++ src/main/java/com/example/springbootdemo/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package com.example.springbootdemo.domain;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+
+@Getter
+@MappedSuperclass // JPA 엔티티 클래스가 BaseTimeEntity를 상속할 경우 필드(createdTime, modifiedTime)도 칼럼으로 인식하도록 함
+@EntityListeners(AuditingEntityListener.class) // BaseTimeEntity 클래스에 Auditing 기능을 포함
+public class BaseTimeEntity {
+    
+    @CreatedDate // 엔티티가 생성되어 저장될 때 시간이 자동 저장
+    private LocalDateTime createdTime;
+
+    @LastModifiedDate // 
+    private LocalDateTime modifiedDate;
+}
