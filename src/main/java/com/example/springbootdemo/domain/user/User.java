98f2161e8049ebfc1ebe8bacc4bcb44e3fb06281
jjh <joojinhyun00@gmail.com>
2022-03-04 01:18:05 +0900

    (domain) User 클래스 생성

---
+++ src/main/java/com/example/springbootdemo/domain/user/User.java
@@ -0,0 +1,59 @@
+package com.example.springbootdemo.domain.user;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import com.example.springbootdemo.domain.BaseTimeEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User extends BaseTimeEntity {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String email;
+
+    @Column
+    private String picture;
+
+    // Enumerated: JPA로 DB 저장 시 Enum 값을 어떤 형태로 저장할 것인가
+    // 기본 타입은 int, 숫자로는 무슨 의미인지 알 수 없기에 문자열(EnumType.STRING)로 변경
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Builder
+    public User(String name, String email, String picture, Role role) {
+        this.name = name;
+        this.email = email;
+        this.picture = picture;
+        this.role = role;
+    }
+
+    public User update(String name, String picture) {
+        this.name = name;
+        this.picture = picture;
+
+        return this;
+    }
+
+    public String getRoleKey() {
+        return this.role.getKey();
+    }
+}
